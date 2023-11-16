74bc47f8fe8b8421fe97bfce4b7e53f3f804e8df
Mindy Wu <mwu084@ucr.edu>
2023-11-16 10:53:43 +0300

    Fixed the paths to the images

--- src/data/FilterableData.ts
+++ src/data/FilterableData.ts
@@ -1,61 +1,61 @@
 export const filterableData = [
     {
         name: "Balerina",
-        src: "./src/images/ballerina.png",
+        src: "./ballerina.png",
         title: "Ballerina",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         name: "Balerina ",
-        src: "./src/images/ballerina 2.png",
+        src: "./ballerina 2.png",
         title: "Ballerina",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         name: "Abstract Art",
-        src: "./src/images/abstract art.png",
+        src: "./abstract art.png",
         title: "Abstract Art",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         name: "Abstract Art",
-        src: "./src/images/abstract art 2.png",
+        src: "./abstract art 2.png",
         title: "Abstract Art",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         name: "Abstract Art",
-        src: "./src/images/abstract art 3.png",
+        src: "./abstract art 3.png",
         title: "Abstract Art",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         name: "Abstract Art",
-        src: "./src/images/abstract art 2.jpg",
+        src: "./abstract art 2.jpg",
         title: "Abstract Art",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         name: "Cyberpunk",
-        src: "./src/images/cyberpunk.jpg",
+        src: "./cyberpunk.jpg",
         title: "Cyberpunk",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         name: "Cyberpunk",
-        src: "./src/images/cyberpunk 2.png",
+        src: "./cyberpunk 2.png",
         title: "Cyberpunk",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         name: "Portrait",
-        src: "./src/images/portrait colors splash.jpg",
+        src: "./portrait colors splash.jpg",
         title: "Portrait",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
         name: "Portrait",
-        src: "./src/images/portrait colors.jpg",
+        src: "./portrait colors.jpg",
         title: "Portrait",
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     }
